47ef34576c3a16ea448cec93f3c5fb515e792237
Parsa Abedi <parsaab03@gmail.com>
2026-09-12 19:38:44 +0330

    feat(axios): configure axios interceptors for authentication

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,10 @@
-import axios, { type AxiosInstance } from "axios";
+import axios, {
+  AxiosError,
+  type AxiosInstance,
+  type InternalAxiosRequestConfig,
+} from "axios";
+
+import { getCookie, setCookie } from "@/shared/utils/cookieHelpers";
 
 import { env } from "@/config/env";
 
@@ -9,5 +15,41 @@ const axiosInstance: AxiosInstance = axios.create({
   headers: { "Content-Type": "application/json" },
   timeout: 10000,
 });
+
+axiosInstance.interceptors.request.use(
+  (request: InternalAxiosRequestConfig) => {
+    const token = getCookie("accessToken");
+    if (token) {
+      request.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return request;
+  },
+  (error) => Promise.reject(error),
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as InternalAxiosRequestConfig & {
+      _retry?: boolean;
+    };
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = getCookie("refreshToken");
+        const { data } = await axios.post(
+          `${BASE_URL}/auth/check-refresh-token`,
+          { refreshToken },
+        );
+        setCookie(data);
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
