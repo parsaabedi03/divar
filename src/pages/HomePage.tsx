3d53becb0f4d2f402ffca4f1d08f02ab671b0c82
Parsa Abedi <parsaab03@gmail.com>
2026-09-19 23:43:00 +0330

    feat(home): add listings layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,11 @@ import { PostCard } from "@/features/posts/components/PostCard";
 
 export const HomePage = () => {
   return (
-    <div>
-      صفحهٔ اصلی
-      <PostCard />
+    <div className="max-w-7xl px-4 mx-auto grid md:grid-cols-5">
+      <div className="col-span-1">side</div>
+      <div className="col-span-4">
+        <PostCard />
+      </div>
     </div>
   );
 };
